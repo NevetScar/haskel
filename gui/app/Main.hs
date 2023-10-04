356da1d4dfc1b7ce33ef57ab1fafef7776e113f7
NevetScar <hstevencg7@gmail.com>
2023-10-04 11:02:03 -0600

    gtk tests added

--- gui/app/Main.hs
+++ gui/app/Main.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Data.GI.Base ( on, AttrOp((:=)), new )
+import qualified GI.Gtk as Gtk
+import GI.Gtk (FileChooserAction(FileChooserActionOpen))
 
 main :: IO ()
-main = someFunc
+main = do
+        Gtk.init Nothing
+        win <- new Gtk.Window [#title := "Choose a file"]
+        fc <- new Gtk.FileChooserDialog [ #action := FileChooserActionOpen ]
+        button <- new Gtk.Button [ #label := "Open dialog"]
+        on button #clicked(putStrLn "Hello world" )
+        on win #destroy Gtk.mainQuit
+        #add win button
+        #showAll win
+        Gtk.main
+        putStrLn "Program finished"
+
+
+trial :: Gtk.FileChooserDialog -> IO FileChooserAction
+trial x = Gtk.fileChooserGetAction x
