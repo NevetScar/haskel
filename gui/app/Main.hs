69a38e6156af317df591f158d3515a3d278b18a8
NevetScar <hstevencg7@gmail.com>
2023-10-04 20:02:05 -0600

    added opening functionality

--- gui/app/Main.hs
+++ gui/app/Main.hs
@@ -4,19 +4,24 @@
 module Main (main) where
 
 import Data.GI.Base ( on )
+import System.Process
 import qualified GI.Gtk as Gtk
 
 main :: IO ()
 main = do
-        Gtk.init Nothing
+        _ <- Gtk.init Nothing
         win <- Gtk.windowNew Gtk.WindowTypeToplevel
         box <- Gtk.boxNew Gtk.OrientationVertical 2
         buttonO <- Gtk.buttonNewWithLabel "Open"
-        fc <- Gtk.fileChooserWidgetNew Gtk.FileChooserActionSelectFolder
-        on win #destroy Gtk.mainQuit
-        Gtk.after buttonO #clicked $ do
+        fc <- Gtk.fileChooserWidgetNew Gtk.FileChooserActionOpen
+        _ <- on win #destroy Gtk.mainQuit
+        _ <- Gtk.after buttonO #clicked $ do
                                       txt <- Gtk.fileChooserGetFilename fc
-                                      print txt
+                                      case txt of
+                                       Nothing -> putStrLn "Nothing selected"
+                                       Just s -> do
+                                                  callCommand $ "xdg-open " ++ show s
+                                                  Gtk.mainQuit
         #add win box
         #add box buttonO
         #add box fc
